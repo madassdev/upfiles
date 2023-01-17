656344533cd9cb6eb7e08d5738db1482589a6be2
Frank Olayinka Favour <favescsskr@gmail.com>
2023-01-17 02:37:15 +0100

    wip: models and migrations

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Item extends Model
 {
     use HasFactory;
+    protected $guarded=[];
+    protected $casts = ['meta' => 'array'];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function children()
+    {
+        return $this->hasMany(Item::class, 'parent_id');
+    }
+
+    public function parent()
+    {
+        return $this->belongsTo(Item::class, 'parent_id');
+    }
 }
--- database/migrations/2023_01_17_011514_create_items_table.php
+++ database/migrations/2023_01_17_011514_create_items_table.php
@@ -15,6 +15,13 @@ return new class extends Migration
     {
         Schema::create('items', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('reference');
+            $table->unsignedBigInteger('parent_id')->nullable();
+            $table->string('name');
+            $table->string('type');
+            $table->text('meta')->nullable();
+            $table->integer('size')->default(0);
             $table->timestamps();
         });
     }
